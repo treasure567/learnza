7ea44477baf31809d19ef09202b304724ac27237
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-07-26 15:41:54 +0100

    Delete register.ts

--- backend/src/rules/register.ts
+++
@@ -1,7 +0,0 @@
-import { ValidationRules } from '../types/validation';
-
-export const registerRules: ValidationRules = {
-    email: 'required|email',
-    name: 'required|string|min:2|max:50',
-    password: 'required|string|min:6|max:50'
-}; 
