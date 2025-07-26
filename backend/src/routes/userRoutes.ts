e73dc3eb150fa46953fa5f87f256487af13aa0aa
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-07-27 00:04:19 +0100

    Update userRoutes.ts

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -3,13 +3,15 @@ import { UserController } from '@controllers/UserController';
 import { authMiddleware } from '@middleware/authMiddleware';
 import { verifiedEmailMiddleware } from '@middleware/verifiedEmailMiddleware';
 import validateRequest from '@middleware/validateRequest';
-import { updateLanguageRules, updateProfileRules, changePasswordRules } from '@rules/user';
+import { updateLanguageRules } from '@rules/user/updateLanguage';
+import { updateAccessibilityRules } from '@rules/user/updateAccessibility';
 
 const router = Router();
 
 router.get('/profile', authMiddleware, verifiedEmailMiddleware, UserController.getProfile);
-router.patch('/profile', authMiddleware, verifiedEmailMiddleware, validateRequest(updateProfileRules), UserController.updateProfile);
-router.patch('/change-password', authMiddleware, verifiedEmailMiddleware, validateRequest(changePasswordRules), UserController.changePassword);
-router.patch('/update-language', authMiddleware, verifiedEmailMiddleware, validateRequest(updateLanguageRules), UserController.updateLanguage);
+router.post('/profile', authMiddleware, UserController.updateProfile);
+router.post('/change-password', authMiddleware, verifiedEmailMiddleware, UserController.changePassword);
+router.post('/update-language', authMiddleware, validateRequest(updateLanguageRules), UserController.updateLanguage);
+router.post('/update-accessibility', authMiddleware, validateRequest(updateAccessibilityRules), UserController.updateAccessibilityNeeds);
 
 export default router; 
