354ffd99fad67b8f98ed8e771cf939a51e676672
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-07-26 23:39:51 +0100

    Update userRoutes.ts

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -3,13 +3,13 @@ import { UserController } from '@controllers/UserController';
 import { authMiddleware } from '@middleware/authMiddleware';
 import { verifiedEmailMiddleware } from '@middleware/verifiedEmailMiddleware';
 import validateRequest from '@middleware/validateRequest';
-import { updateLanguageRules } from '@rules/user/updateLanguage';
+import { updateLanguageRules, updateProfileRules, changePasswordRules } from '@rules/user';
 
 const router = Router();
 
 router.get('/profile', authMiddleware, verifiedEmailMiddleware, UserController.getProfile);
-router.put('/profile', authMiddleware, UserController.updateProfile);
-router.post('/change-password', authMiddleware, verifiedEmailMiddleware, UserController.changePassword);
-router.put('/update-language', authMiddleware, validateRequest(updateLanguageRules), UserController.updateLanguage);
+router.patch('/profile', authMiddleware, verifiedEmailMiddleware, validateRequest(updateProfileRules), UserController.updateProfile);
+router.patch('/change-password', authMiddleware, verifiedEmailMiddleware, validateRequest(changePasswordRules), UserController.changePassword);
+router.patch('/update-language', authMiddleware, verifiedEmailMiddleware, validateRequest(updateLanguageRules), UserController.updateLanguage);
 
 export default router; 
