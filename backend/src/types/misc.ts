183a4dd9ffbb7b359a0834a0abadb15b54a17685
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-07-27 00:04:22 +0100

    Update misc.ts

--- backend/src/types/misc.ts
+++ backend/src/types/misc.ts
@@ -12,4 +12,17 @@ export interface ILanguage extends Language, Document {}
 
 export interface LanguageResponse {
     languages: Language[];
+}
+
+export interface Accessibility {
+    value: string;
+    name: string;
+    description: string;
+    isActive?: boolean;
+}
+
+export interface IAccessibility extends Accessibility, Document {}
+
+export interface AccessibilityResponse {
+    accessibilities: Accessibility[];
 } 
