b8eee935b92d7546dbd2b28cefa099c2e9b34564
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-07-27 00:04:19 +0100

    Update miscRoutes.ts

--- backend/src/routes/miscRoutes.ts
+++ backend/src/routes/miscRoutes.ts
@@ -4,5 +4,6 @@ import { MiscController } from '@controllers/MiscController';
 const router = Router();
 
 router.get('/languages', MiscController.getLanguages);
+router.get('/accessibilities', MiscController.getAccessibilities);
 
 export default router; 
