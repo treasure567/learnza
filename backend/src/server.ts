eedd3da7c1929c9d58dbdf68c0501492af0a7b9f
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-07-26 22:42:32 +0100

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import express, { Application } from 'express';
 import mongoose from 'mongoose';
 import authRoutes from '@/routes/authRoutes';
 import userRoutes from '@/routes/userRoutes';
+import miscRoutes from '@/routes/miscRoutes';
 import { errorHandler, notFoundHandler } from '@middleware/errorHandler';
 
 dotenv.config();
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
+app.use('/api/misc', miscRoutes);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
