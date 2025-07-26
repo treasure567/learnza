79bdd500b6ccecb32251a126bbc2e07cc0833885
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-07-26 15:41:51 +0100

    Update authRoutes.ts

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
-import { AuthController } from '../controllers/AuthController';
-import validateRequest from '../middleware/validateRequest';
-import { registerRules } from '../rules/register';
-import { loginRules } from '../rules/login';
+import { AuthController } from '@controllers/AuthController';
+import validateRequest from '@middleware/validateRequest';
+import { authMiddleware } from '@middleware/authMiddleware';
+import { registerRules, loginRules, verifyEmailRules } from '@rules/auth';
 
 const router = Router();
 
 router.post('/register', validateRequest(registerRules), AuthController.register);
 router.post('/login', validateRequest(loginRules), AuthController.login);
+router.post('/verify-email', authMiddleware, validateRequest(verifyEmailRules), AuthController.verifyEmail);
+router.post('/resend-verification', authMiddleware, AuthController.resendVerificationCode);
 
 export default router; 
