01cdc04e7d7493f17236b782320470a0460f1177
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-07-26 15:41:57 +0100

    Update user.ts

--- backend/src/types/user.ts
+++ backend/src/types/user.ts
@@ -4,6 +4,8 @@ export interface IUser extends Document {
     email: string;
     name: string;
     password: string;
+    verificationCode?: string;
+    email_verified_at: Date | null;
     createdAt: Date;
     updatedAt: Date;
 }
@@ -11,4 +13,8 @@ export interface IUser extends Document {
 export interface UserResponse {
     user: IUser;
     token: string;
+}
+
+export interface VerifyEmailRequest {
+    code: string;
 } 
