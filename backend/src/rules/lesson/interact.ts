ee0f22a992f1aae3e2f9ae7e94fa36ae4a92fe65
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-07-28 13:51:23 +0100

    Update interact.ts

--- backend/src/rules/lesson/interact.ts
+++ backend/src/rules/lesson/interact.ts
@@ -1,5 +1,6 @@
 import { ValidationRules } from '@/types/validation';
 
 export const interactRules: ValidationRules = {
-    message: 'required|string|min:1|max:1000'
+    message: 'required|string|min:1|max:1000',
+    lessonId: 'required|string|min:1|max:1000'
 }; 
