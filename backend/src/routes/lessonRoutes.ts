730055366bc4db461200c918f4b52bd30bd81f6f
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-07-28 11:33:03 +0100

    Update lessonRoutes.ts

--- backend/src/routes/lessonRoutes.ts
+++ backend/src/routes/lessonRoutes.ts
@@ -1,17 +1,14 @@
 import { Router } from 'express';
 import { LessonController } from '@controllers/LessonController';
 import { authMiddleware } from '@middleware/authMiddleware';
-import { verifiedEmailMiddleware } from '@middleware/verifiedEmailMiddleware';
 import validateRequest from '@middleware/validateRequest';
-import { generateLessonRules } from '@rules/lesson';
+import { generateLessonRules, interactRules } from '@rules/lesson';
 
 const router = Router();
 
-router.use(authMiddleware);
-router.use(verifiedEmailMiddleware);
-
-router.get('/', LessonController.getLessons);
-router.get('/:id', LessonController.getLesson);
-router.post('/generate', validateRequest(generateLessonRules), LessonController.generateLesson);
+router.get('/', authMiddleware, LessonController.getLessons);
+router.get('/:id', authMiddleware, LessonController.getLesson);
+router.post('/generate', authMiddleware, validateRequest(generateLessonRules), LessonController.generateLesson);
+router.post('/interact', authMiddleware, validateRequest(interactRules), LessonController.interact);
 
 export default router; 
