4505889f2af72c475a7d42888985e66dbaa8d4b9
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-07-30 10:15:25 +0100

    Delete backend directory

--- backend/src/rules/auth/forgotPassword.ts
+++
@@ -1,5 +0,0 @@
-import { ValidationRules } from '@/types/validation';
-
-export const forgotPasswordRules: ValidationRules = {
-    email: 'required|email'
-}; 
--- backend/src/rules/lesson/index.ts
+++
@@ -1,2 +0,0 @@
-export { generateLessonRules } from './generate';
-export { interactRules } from './interact';
