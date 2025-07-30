e86440a40a9b40b20d6f31060cdf1648549f7be4
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-07-30 07:51:32 +0100

    dd

--- backend/src/rules/auth/resetPassword.ts
+++
@@ -1,6 +0,0 @@
-import { ValidationRules } from '@/types/validation';
-
-export const resetPasswordRules: ValidationRules = {
-    token: 'required|string',
-    password: 'required|string|min:6|max:50'
-}; 
--- backend/src/utils/UserUtils.ts
+++
@@ -1,8 +0,0 @@
-import { IUser } from '@/types/user';
-import { Document } from 'mongoose';
-
-export class UserUtils {
-    static async populateUser(user: Document & IUser): Promise<IUser> {
-        return user.populate(['language', 'accessibilityNeeds']);
-    }
-} 
