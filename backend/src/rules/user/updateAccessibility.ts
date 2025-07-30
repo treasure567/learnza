9112f902ff06ee741202474a13e085d54eb842b1
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2025-07-30 07:54:18 +0100

    dd

--- backend/src/rules/user/updateAccessibility.ts
+++
@@ -1,5 +0,0 @@
-import { ValidationRules } from '@/types/validation';
-
-export const updateAccessibilityRules: ValidationRules = {
-    accessibilityIds: 'required|array'
-}; 
--- frontend/app/components/ui/card.tsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="bg-light rounded-[24px] p-5 animate-float shadow-lg">
-      {children}
-    </div>
-  );
-}
